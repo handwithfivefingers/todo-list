08737524fe0d51fa2c78b21f134879f22e91d9ca
Handwithfivefingers <handgod1995@gmail.com>
2021-07-26 21:55:13 +0700

    url

--- src/helper/AxiosService.js
+++ src/helper/AxiosService.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 const instance = axios.create({
-  // baseURL: `${
-  //   process.env.REACT_APP_API_GLOBAL || process.env.REACT_APP_API_LOCAL
-  // }`,
-  baseURL: `${process.env.REACT_APP_API_LOCAL}`,
+  baseURL: `${
+    process.env.REACT_APP_API_GLOBAL || process.env.REACT_APP_API_LOCAL
+  }`,
+  // baseURL: `${process.env.REACT_APP_API_LOCAL}`,
 });
 export default instance;
