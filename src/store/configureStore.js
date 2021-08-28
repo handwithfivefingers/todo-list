2e0debc51fed85fe933bb2310032289ae6b4bded
Truyenmv <truyenmv>
2021-08-28 17:54:04 +0700

    ... store config

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -6,10 +6,7 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 const middlewares = [thunk];
 const middlewareEnhancer = applyMiddleware(...middlewares);
 const enhancers = [middlewareEnhancer];
-// const composeEnhancers = composeWithDevTools(...enhancers);
-const composeEnhancers = composeWithDevTools({
-
-});
-const store = createStore(rootReducer, composeEnhancers(middlewareEnhancer));
+const composeEnhancers = composeWithDevTools(...enhancers);
+const store = createStore(rootReducer, composeEnhancers);
 export default store;
 
